54cfed8f2ad0b2edd974f18adead17c5009124a1
Ariess Lin <ariess.lin@thoughtworks.com>
2023-12-04 20:16:05 +0100

    day 2 part 2

--- day2.js
+++ day2.js
@@ -31,14 +31,6 @@ function parseGameString(gameString) {
     return { id, sets };
 }
 
-function calculatePossibleGamesSum(gameStrings) {
-    const games = gameStrings.map(parseGameString);
-
-    return games
-        .filter((game) => game.sets.every(verifySet))
-        .reduce((sum, game) => sum + game.id, 0);
-}
-
 function verifySet(set) {
     for (const color in set) {
         if (bag[color] < set[color]) {
@@ -48,14 +40,58 @@ function verifySet(set) {
     return true;
 }
 
+function calculatePossibleGamesSum(gameStrings) {
+    const games = gameStrings.map(parseGameString);
+
+    return games
+        .filter((game) => game.sets.every(verifySet))
+        .reduce((sum, game) => sum + game.id, 0);
+}
+
+function calculateMinCubesForGame(game) {
+    const minCubes = { red: 0, green: 0, blue: 0 };
+
+    game.sets.forEach((set) => {
+        for (const color in set) {
+            if (set[color] > minCubes[color]) {
+                minCubes[color] = set[color];
+            }
+        }
+    });
+
+    return minCubes;
+}
+
+function calculatePowerOfSet(minCubes) {
+    return Object.values(minCubes).reduce(
+        (product, count) => product * count,
+        1
+    );
+}
+
+function calculateMinimumCubesPowerSum(gameStrings) {
+    const games = gameStrings.map(parseGameString);
+
+    return games.reduce((totalSum, game) => {
+        if (game.id === null) return totalSum;
+        const minCubes = calculateMinCubesForGame(game);
+        const power = calculatePowerOfSet(minCubes);
+        return totalSum + power;
+    }, 0);
+}
+
 function main() {
     const filePath = "day2_input.txt";
     const lines = processFile(filePath);
 
-    const result = calculatePossibleGamesSum(lines);
+    const result = calculateMinimumCubesPowerSum(lines);
     console.log("Sum of possible games:", result);
 }
 
 main();
 
-module.exports = { parseGameString, calculatePossibleGamesSum };
+module.exports = {
+    parseGameString,
+    calculatePossibleGamesSum,
+    calculateMinimumCubesPowerSum,
+};
--- day2.test.js
+++ day2.test.js
@@ -1,4 +1,8 @@
-const { parseGameString, calculatePossibleGamesSum } = require("./day2");
+const {
+    parseGameString,
+    calculatePossibleGamesSum,
+    calculateMinimumCubesPowerSum,
+} = require("./day2");
 
 describe("day 2 part 1 - helper - parseGameString", () => {
     it("correctly parses a game string with multiple colors and sets", () => {
@@ -42,14 +46,14 @@ describe("day 2 part 1 - calculatePossibleGamesSum", () => {
             "Game 1: 3 green, 4 red; 1 red, 2 green, 6 blue; 2 green",
             "Game 2: 1 blue, 2 green; 3 green, 4 red, 1 blue; 1 green, 1 blue",
         ];
-        expect(calculatePossibleGamesSum(games)).toBe(3); // IDs 1 and 2 are possible
+        expect(calculatePossibleGamesSum(games)).toBe(3);
     });
 
     it("should return zero for games with impossible subsets", () => {
         const games = [
             "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
         ];
-        expect(calculatePossibleGamesSum(games)).toBe(0); // Game 3 is impossible
+        expect(calculatePossibleGamesSum(games)).toBe(0);
     });
 
     it("should return zero for an empty list of games", () => {
@@ -63,5 +67,38 @@ describe("day 2 part 1 - calculatePossibleGamesSum", () => {
             "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
         ];
         expect(calculatePossibleGamesSum(games)).toBe(6); // Only games 1 and 5 are possible
+    });
+});
+
+describe("day 2 part 2 calculateMinimumCubesPowerSum", () => {
+    it("calculates the power of the minimum set for a single game 1", () => {
+        const games = [
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        ];
+        expect(calculateMinimumCubesPowerSum(games)).toEqual(48);
+    });
+
+    it("calculates the power of the minimum set for a single game 2", () => {
+        const games = [
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        ];
+        expect(calculateMinimumCubesPowerSum(games)).toEqual(1560);
+    });
+
+    it("calculates the total power for multiple games", () => {
+        const games = [
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        ];
+        expect(calculateMinimumCubesPowerSum(games)).toEqual(48); // Total power for Games 2 and 5
+    });
+
+    it("handles an empty game list", () => {
+        expect(calculateMinimumCubesPowerSum([])).toEqual(0);
+    });
+
+    it("ignores invalid game strings", () => {
+        const games = ["Invalid Game String"];
+        expect(calculateMinimumCubesPowerSum(games)).toEqual(0);
     });
 });
